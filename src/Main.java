16dc09109ddeb5d40b98786af1f909ffdcc4cd67
RizhiyBess <m_m16@mail.ru>
2025-09-01 07:59:31 +0300

    Исправленное ДЗ-5 (2)

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,24 @@
 public class Main {
     public static void main(String[] args) {
         int clientOS = 0;
-        int clientDeviceYear = 2025;
+        int clientDeviceYear = 2014;
+
         if (clientOS == 0) {
             if (clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
             } else {
-                System.out.println("Установите обычную версию приложения для iOS по ссылке");
+                System.out.println("Установите версию приложения для iOS по ссылке");
             }
         } else if (clientOS == 1) {
             if (clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
             } else {
-                System.out.println("Установите обычную версию приложения для Android по ссылке");
+                System.out.println("Установите версию приложения для Android по ссылке");
             }
         }
 
         int year = 1587;
-        if (year < 1584) {
-            System.out.println(year + " год не учитывается");
-        } else if (year >= 1584) {
+        if (year >= 1584) {
             if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
                 System.out.println(year + " год является високосным");
             } else {
@@ -45,22 +44,22 @@ public class Main {
             case 12:
             case 1:
             case 2:
-                System.out.println("Зимний месяц");
+                System.out.println("Зима");
                 break;
             case 3:
             case 4:
             case 5:
-                System.out.println("Весений месяц");
+                System.out.println("Весна");
                 break;
             case 6:
             case 7:
             case 8:
-                System.out.println("Летний месяц");
+                System.out.println("Лето");
                 break;
             case 9:
             case 10:
             case 11:
-                System.out.println("Осений месяц");
+                System.out.println("Осень");
                 break;
             default:
                 System.out.println("Такого месяца не существует");
